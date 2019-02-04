5939cb8a39bbb85d434904effe5353726a1933ef
eitan613 <eitanadler613@gmail.com>
2019-02-04 06:41:34 -0500

    fixed some issues

--- src/HumanPaddle.java
+++ src/HumanPaddle.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class HumanPaddle implements Paddle {//stop hp from flickering
+public class HumanPaddle implements Paddle {
     private int y, x;
 
     public HumanPaddle() {
@@ -18,14 +18,8 @@ public class HumanPaddle implements Paddle {//stop hp from flickering
     public void move(int n, Graphics g, Color c) {
         g.setColor(c);
         g.fillRect(x,y,20,80);
+        y = n-40;
         g.setColor(Color.black);
-        if(n < 0){
-            y -= n;
-        }
-        else{
-            y -= n;
-        }
-
         if(y <  75)
             y = 75;
         else if(y > 420)
--- src/Pong.java
+++ src/Pong.java
@@ -1,20 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+import java.awt.event.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
 
-public class Pong extends JFrame implements MouseWheelListener {
-    Ball b;
-    ComputerPaddle Cp;
-    HumanPaddle hp;
+public class Pong extends JFrame{
+    private Ball b;
+    private ComputerPaddle Cp;
+    private HumanPaddle hp;
+    private Timer ballTimer;
     private JButton playAgain = new JButton("Click here to play again.");
-    private int notches;
 
     public static void main(String[] args) throws ClassNotFoundException {
         Pong pong = new Pong();
@@ -25,7 +22,16 @@ public class Pong extends JFrame implements MouseWheelListener {
         add(BorderLayout.SOUTH,playAgain);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         hp = new HumanPaddle();
-        addMouseWheelListener(this);
+        addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {}
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                int y = e.getY();
+                hp.move(y, getGraphics(), getBackground());
+            }
+        });
         b = new Ball();
         Cp = new ComputerPaddle(getWidth(), getHeight());
         b.ball(getHeight());
@@ -44,69 +50,45 @@ public class Pong extends JFrame implements MouseWheelListener {
         add(BorderLayout.NORTH, statusBar);
         this.setVisible(true);
         highScore Hs = hs;
-        playAgain.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                b.setXCenter();
-                b.setYCenter();
-                status.setText("You have " + Hs.getPoints() + " points");
-                Hs.setHighScoreText(null);
-                ballTimer.start();
-            }
+        playAgain.addActionListener(e -> {
+            b.setXCenter();
+            b.setYCenter();
+            status.setText("You have " + Hs.getPoints() + " points");
+            Hs.setHighScoreText(null);
+            ballTimer.start();
         });
-            ballTimer = new Timer(100, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    playAgain.setEnabled(false);
-                    b.move(getGraphics(), getBackground());
-                    Cp.move(b.getY(), getGraphics(), getBackground());
-                    if (b.getX() == 50 && (b.getY() - 10 >= hp.getY() && b.getY() + 10 <= hp.getY() + 80)) {
-                        b.changeDirection();
-                        Hs.incrementPoints();
-                        status.setText("You have " + Hs.getPoints() + " points");
-                    }
-                    if (b.getX() == getWidth() - 50)
-                        b.changeDirection();
-                    if (b.getX() < -10) {
-                        try {
-                            ballTimer.stop();
-                            playAgain.setEnabled(true);
-                            Hs.endGame();
-                            //have hs textArea added to this frame instead of new, with no button-joption pane instead
-                            //stop hp from flickering
-                            //have hp move based on mouse not mouse wheel
-                            //improve ball to speed up & change more than just xVelocity
-                        } catch (IOException e1) {
-                            e1.printStackTrace();
-                        }
+            ballTimer = new Timer(100, e -> {
+                playAgain.setEnabled(false);
+                b.move(getGraphics(), getBackground());
+                Cp.move(b.getY(), getGraphics(), getBackground());
+                if (b.getX() == 50 && (b.getY() - 10 >= hp.getY() && b.getY() + 10 <= hp.getY() + 80)) {
+                    b.changeDirection();
+                    Hs.incrementPoints();
+                    status.setText("You have " + Hs.getPoints() + " points");
+                }
+                if (b.getX() == getWidth() - 50)
+                    b.changeDirection();
+                if (b.getX() < -10) {
+                    try {
+                        ballTimer.stop();
+                        playAgain.setEnabled(true);
+                        Hs.endGame();
+                        /**
+                         * TODO have hs textArea added to this frame instead of new, with no button-joption pane instead
+                         *  improve ball to speed up & change more than just xVelocity
+                         *  fix up code-better practices
+                         */
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
                     }
                 }
             });
             ballTimer.start();
     }
-
-
-    Timer ballTimer;
     public void paint(Graphics g) {
         super.paint(g);
         hp.draw(g);
         b.draw(g);
         Cp.draw(g);
-    }
-
-
-    
-
-    Timer scrollTimer;
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e) {
-        notches = e.getWheelRotation();
-        scrollTimer = new Timer(10, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                hp.move(notches, getGraphics(), getBackground());
-            }
-        });
-        scrollTimer.start();
     }
 }
